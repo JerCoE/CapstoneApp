3c8e25c10461c6e5ddce6f50e7096763e9fbb5c0
Dann Jewel M De Jesus <comeforthstratos@gmail.com>
2025-10-10 14:50:51 +0800

    Merge Login UI

--- src/Frontend/components/LoginScreen.tsx
+++ src/Frontend/components/LoginScreen.tsx
@@ -89,39 +89,6 @@ const LoginScreen: React.FC = () => {
   };
 
   return (
-    <div className="Pausepoint"><span className="pausepoint-text">PausePoint</span>
-    <div className="BackgroundLogo">
-       <div className="BGtext">
-          <div className="login-container">
-           <h2 style={{ textAlign: 'center', fontWeight: '600', color: '#113372' }}>Login</h2>
-             <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="email">Email</label>
-            <input
-              className="input-field"
-              type="text"
-              id="email"
-              placeholder="employee@company.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input
-              className="input-field"
-              type="password"
-              id="password"
-              placeholder="Enter your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <button type="submit">Enter</button>
-         </form>
-        </div>
     <div className="Pausepoint">
       <span className="pausepoint-text">PausePoint</span>
       <div className="BackgroundLogo">
@@ -143,8 +110,8 @@ const LoginScreen: React.FC = () => {
               </button>
             </div>
           </div>
+        </div>
       </div>
-     </div>
     </div>
   );
 };
