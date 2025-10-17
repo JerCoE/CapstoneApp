90ac0690ccb09bbf95b8884635ae6edddc5a191e
JerCoE <jeremy.cobar@betterask.erni>
2025-10-17 11:53:12 +0800

    Remove ButtonRequest on employeedashboard

--- src/Frontend/pages/Employee/EmployeeDashboard.tsx
+++ src/Frontend/pages/Employee/EmployeeDashboard.tsx
@@ -14,7 +14,6 @@ export default function DashboardLayout({ onLogout, userEmail }: { onLogout?: ()
       
       <main className="dashboard-main">
         <Outlet />
-        <ButtonRequest />
         
    
       </main>
